807fc7dae8d054ea72c7f0bd686c4c8e0e72dc2e
Your Name <levente.szomor@gmail.com>
2026-06-17 21:31:15 +0200

    urls: add reload

--- a_core/urls.py
+++ a_core/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from a_posts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view)
+    path('', home_view),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
