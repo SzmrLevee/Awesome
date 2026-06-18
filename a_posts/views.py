cbcaffa29daad98db4dd1632c5caeecdf3946a7f
Your Name <levente.szomor@gmail.com>
2026-06-18 22:28:55 +0200

    Debugging with print

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def home_view(request):
+    #print(request.META)
+    #print(request.method)
+    
     #title = 'Awesome Photos & Captions'
     #return render(request, 'index.html', {'title': title})
+    
     return render(request, 'includes/a_posts/home.html')
