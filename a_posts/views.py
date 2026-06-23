714f0eae238c6c9a8f88305ad98b373dd3c9f578
Your Name <levente.szomor@gmail.com>
2026-06-23 16:29:07 +0200

    Webcrawler finalization

--- a_posts/views.py
+++ a_posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.forms import ModelForm
 from django import forms
+from bs4 import BeautifulSoup
+import requests
 
 def home_view(request):
     #print(request.META)
@@ -31,7 +33,24 @@ def post_create_view(request):
     if request.method=='POST':
         form=PostCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            post=form.save(commit=False)
+            
+            website = requests.get(form.data['url'])
+            sourcecode = BeautifulSoup(website.text, 'html.parser')
+
+            find_image = sourcecode.select('meta[content^="https://live.staticflickr.com/"]')
+            image = find_image[0]['content']
+            post.image = image
+            
+            find_title = sourcecode.select('h1.photo-title')
+            title = find_title[0].text.strip()
+            post.title = title
+            
+            find_artist = sourcecode.select('a.owner-name')
+            artist = find_artist[0].text.strip()
+            post.artist = artist
+            
+            post.save()
             return redirect('home')
     
     return render(request, 'a_posts/post_create.html', {'form':form})
