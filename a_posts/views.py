ddc42ffebb90afcaff9089f0d76afcab30ff8c4f
Your Name <levente.szomor@gmail.com>
2026-06-23 15:59:07 +0200

    Url field

--- a_posts/views.py
+++ a_posts/views.py
@@ -16,12 +16,13 @@ def home_view(request):
 class PostCreateForm(ModelForm):
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['url', 'body']
         labels = {
             'body':'Caption',
         }
         widgets = {
-            'body': forms.Textarea(attrs={'row': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
+            'body' : forms.Textarea(attrs={'row': 3, 'placeholder': 'Add a caption ...', 'class': 'font1 text-4xl'}),
+            'url' : forms.TextInput(attrs={'placeholder': 'Add url ...'}),
         }
 
 def post_create_view(request):
