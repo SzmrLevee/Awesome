59a6f637b5fddad4d3472b85a2f51f5fbe64f254
Levente Szomor <levente.szomor@gmail.com>
2026-07-09 18:51:36 +0200

    Privacy & Permissions: Backend

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 from bs4 import BeautifulSoup
@@ -28,6 +29,7 @@ def home_view(request, tag=None):
     
     return render(request, 'a_posts/home.html', context)
 
+@login_required
 def post_create_view(request):
     form=PostCreateForm()
     
@@ -59,9 +61,10 @@ def post_create_view(request):
     
     return render(request, 'a_posts/post_create.html', {'form' : form})
 
+@login_required
 def post_delete_view(request, pk):
     #post = Post.objects.get(id=pk)
-    post = get_object_or_404(Post, id=pk)
+    post = get_object_or_404(Post, id=pk, author=request.user)
     
     if request.method == "POST":
         post.delete()
@@ -70,9 +73,10 @@ def post_delete_view(request, pk):
     
     return render(request, 'a_posts/post_delete.html', {'post': post})
 
+@login_required
 def post_edit_view(request, pk):
     #post = Post.objects.get(id=pk)
-    post = get_object_or_404(Post, id=pk)
+    post = get_object_or_404(Post, id=pk, author=request.user)
     form = PostEditForm(instance=post)
     
     if request.method == "POST":
--- a_users/views.py
+++ a_users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
@@ -16,6 +17,7 @@ def profile_view(request, username=None):
             raise Http404()
     return render(request, 'a_users/profile.html', {'profile' : profile})
 
+@login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
 
@@ -27,6 +29,7 @@ def profile_edit_view(request):
 
     return render(request, 'a_users/profile_edit.html', {'form' : form})
 
+@login_required
 def profile_delete_view(request):
 
     user = request.user
