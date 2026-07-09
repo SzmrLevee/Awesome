276c2d5fa85e597364a9e1af6da6fab4140c1919
Levente Szomor <levente.szomor@gmail.com>
2026-07-09 15:44:28 +0200

    Represent with the user

--- a_users/models.py
+++ a_users/models.py
@@ -9,3 +9,6 @@ class Profile(models.Model):
     location = models.CharField(max_length=20, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
