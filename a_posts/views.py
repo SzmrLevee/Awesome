db4f26ff1dd6046cf7253b832029b6cc84a7f021
Levente Szomor <levente.szomor@gmail.com>
2026-07-09 18:42:35 +0200

    Post: author

--- a_posts/views.py
+++ a_posts/views.py
@@ -50,6 +50,8 @@ def post_create_view(request):
             find_artist = sourcecode.select('a.owner-name')
             artist = find_artist[0].text.strip()
             post.artist = artist
+
+            post.author = request.user
             
             post.save()
             form.save_m2m()
