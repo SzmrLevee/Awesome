6fdad7a950565f75cb5060274bb4fdec70ae3733
Levente Szomor <levente.szomor@gmail.com>
2026-07-10 15:35:52 +0200

    Profile email edit fix

--- a_users/signals.py
+++ a_users/signals.py
@@ -1,5 +1,6 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -11,3 +12,11 @@ def create_profile(sender, instance, created, **kwargs):
             user = user,
             email = user.email
         )
+
+@receiver(post_save, sender=Profile)
+def update_user(sender, instance, created, **kwargs):    
+    profile = instance    
+    if created == False:       
+        user = get_object_or_404(User, id=profile.user.id)           
+        user.email = profile.email
+        user.save()
