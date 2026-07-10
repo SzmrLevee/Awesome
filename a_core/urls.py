c06f38ab9929db8d46c9932ac631b1421d1e3053
Levente Szomor <levente.szomor@gmail.com>
2026-07-10 17:41:36 +0200

    Delete comment and delete button visibility

--- a_core/urls.py
+++ a_core/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path('profile/delete/', profile_delete_view, name='profile-delete'),
     path('profile/onboarding/', profile_edit_view, name='profile-onboarding'),
     path('commentsent/<pk>/', comment_sent, name='comment-sent'),
+    path('comment/delete/<pk>/', comment_delete_view, name='comment-delete'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
--- a_posts/views.py
+++ a_posts/views.py
@@ -117,3 +117,14 @@ def comment_sent(request, pk):
             comment.parent_post = post
             comment.save()
     return redirect('post', post.id)
+
+@login_required
+def comment_delete_view(request, pk):
+    post = get_object_or_404(Comment, id=pk, author=request.user)
+
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, 'Comment deleted')
+        return redirect('post', post.parent_post.id)
+
+    return render(request, 'a_posts/comment_delete.html', {'comment' : post})
