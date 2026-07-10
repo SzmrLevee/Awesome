92c458f032100d1c787e46a4a94a5f197d9d9520
Levente Szomor <levente.szomor@gmail.com>
2026-07-10 16:44:56 +0200

    Basic Comment model logic

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,3 +1,4 @@
+from io import open_code
 from django.db import models
 from django.contrib.auth.models import User
 import uuid
@@ -30,3 +31,13 @@ class Tag(models.Model):
     
     class Meta:
         ordering = ['order']
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comments')
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.body[:30]}'
