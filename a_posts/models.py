457d8689e575d65fa41eeebf65ef89ecc0420428
Levente Szomor <levente.szomor@gmail.com>
2026-07-10 17:20:25 +0200

    Comments ordering

--- a_posts/models.py
+++ a_posts/models.py
@@ -44,3 +44,6 @@ class Comment(models.Model):
             return f'{self.author.username} : {self.body[:30]}'
         except:
             return f'no author : {self.body[:30]}'
+
+    class Meta:
+        ordering = ['-created']
